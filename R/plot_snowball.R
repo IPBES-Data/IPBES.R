33c4a60f3f25b4222f2d047e524968484e6386a6
Rainer M. Krug <Rainer@krugs.de>
2023-10-02 15:23:15 +0200

    Some changes

--- R/plot_snowball.R
+++ R/plot_snowball.R
@@ -1,4 +1,3 @@
-
 #' @title FUNCTION_TITLE
 #' @description FUNCTION_DESCRIPTION
 #' @param snowball PARAM_DESCRIPTION
@@ -6,18 +5,18 @@
 #' @param path PARAM_DESCRIPTION, Default: 'figures'
 #' @return OUTPUT_DESCRIPTION
 #' @details DETAILS
-#' @examples 
+#' @examples
 #' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
+#' if (interactive()) {
+#'     # EXAMPLE1
 #' }
-#' @seealso 
+#' }
+#' @seealso
 #'  \code{\link[tidygraph]{as_tbl_graph.data.frame}}
 #'  \code{\link[ggraph]{ggraph}}, \code{\link[ggraph]{geom_edge_link}}, \code{\link[ggraph]{geom_node_point}}, \code{\link[ggraph]{geom_node_text}}, \code{\link[ggraph]{theme_graph}}
 #'  \code{\link[ggplot2]{aes}}, \code{\link[ggplot2]{character(0)}}, \code{\link[ggplot2]{scale_size}}, \code{\link[ggplot2]{scale_manual}}, \code{\link[ggplot2]{theme}}, \code{\link[ggplot2]{ggsave}}, \code{\link[ggplot2]{margin}}, \code{\link[ggplot2]{c("guide_bins", "guide_colourbar", "guide_coloursteps", "guide_legend", "guides", "guides")}}, \code{\link[ggplot2]{labs}}
 #' @rdname plot_snowball
-#' @export 
+#' @export
 #' @importFrom tidygraph as_tbl_graph
 #' @importFrom ggraph ggraph geom_edge_link geom_node_point geom_node_label theme_graph scale_edge_width
 #' @importFrom ggplot2 aes scale_size scale_fill_manual theme ggsave element_rect guides ggtitle
@@ -41,7 +40,7 @@ plot_snowball <- function(snowball, name, path = "figures") {
             legend.position = "bottom"
         ) +
         ggplot2::guides(fill = "none") +
-            ggplot2::ggtitle(paste0(name, " Cited by average count per year"))
+        ggplot2::ggtitle(paste0(name, " Cited by average count per year"))
 
     ggplot2::ggsave(file.path(path, paste0(name, "_cited_by_count_by_year.pdf")), plot = p_cby, device = cairo_pdf, width = 20, height = 15)
     ggplot2::ggsave(file.path(path, paste0(name, "_cited_by_count_by_year.png")), plot = p_cby, width = 20, height = 15, bg = "white")
@@ -63,10 +62,10 @@ plot_snowball <- function(snowball, name, path = "figures") {
             legend.position = "bottom"
         ) +
         ggplot2::guides(fill = "none") +
-            ggplot2::ggtitle(paste0(name, " Cited by count"))
+        ggplot2::ggtitle(paste0(name, " Cited by count"))
 
     ggplot2::ggsave(file.path(path, paste0(name, "_cited_by_count.pdf")), plot = p_cb, device = cairo_pdf, width = 20, height = 15)
     ggplot2::ggsave(file.path(path, paste0(name, "_cited_by_count.png")), plot = p_cb, width = 20, height = 15, bg = "white")
 
-    return(list(size_by_count = p_cb, size_by_count_per_year = p_cby)
+    return(list(size_by_count = p_cb, size_by_count_per_year = p_cby))
 }
